6a5f12edfe023016c66de848d56ebfb9199d3f00
Ramesh-Seervi <ramesh@imcrinox.com>
2026-02-21 19:32:28 +0530

    new chnages

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.get('/', (req, res) => {
     res.send('API is running...');
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5005;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
